c850315567757367d3aaf0e3222015d91f30cae7
Mikael Carvalho <mcb3@discente.ifpe.edu.br>
2021-10-26 16:22:33 -0300

    Final commit

--- lib/new_page.dart
+++ lib/new_page.dart
@@ -18,10 +18,25 @@ class _NewPageState extends State<NewPage> {
       body: ListView.builder(
         itemCount: widget.values.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text( widget.values[index]['nome']),
-            subtitle: Text(widget.values[index]['idade']),
-          );
+          return Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: ListTile(
+                title:Text(widget.values[index]['nome']),
+                subtitle: Text(widget.values[index]['idade']),
+                leading: Icon(Icons.person),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10),
+                  ),
+                  side: BorderSide(
+                    color: Colors.blue,
+                    width: 2,
+                  ),
+                ),
+              ));
         },
       ),
     );
